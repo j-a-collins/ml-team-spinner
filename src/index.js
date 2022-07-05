e667cc78872d3554c4f882a8ac31e57bb111ea01
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2022-07-05 20:53:20 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Wheel from "./components/wheel";
+
+import "./styles.css";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.places = ["Jack C", "Jack F", "Aaron", "Dan", "Matt", "Martin", "Ben"];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Who should lead the next stand-up?</h1>
+        <Wheel items={this.places} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
